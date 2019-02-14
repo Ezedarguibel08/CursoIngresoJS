bc6b1ebffe4b00c5aa22d3c91c900b741e80ec56
Ezedarguibel08 <47418117+Ezedarguibel08@users.noreply.github.com>
2019-02-14 12:17:15 -0300

    tps

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,38 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
-
+    var ancho
+    var largo
+    largo=document.getElementById("Largo").value;
+    ancho=document.getElementById("Ancho").value;
+    largo=parseInt(largo);
+    ancho=parseInt(ancho);
+    alert("Necesitas "+ [ancho*3+largo*3]+" metros de alambre");
 }
 function Circulo () 
 {
+    var radio;
+    var area;
+    radio=document.getElementById("Radio").value;
+    radio=parseInt(radio);
+    area=radio*radio;
+    area=parseInt(area);
+    alert("Necesitas"+[area*3]+" metros de alambre");
 	
 }
 function Materiales () 
 {
-	
+    var largo;
+    var ancho;
+    var cal;
+    var cemento;
+    largo=document.getElementById("Largo").value;
+    ancho=document.getElementById("Ancho").value;
+    largo=parseInt(largo);
+    ancho=parseInt(ancho);
+    cal=(largo*3+ancho*3)*2;
+    cemento=(largo*2+ancho*2)*2;
+    cal=parseInt(cal);
+    cemento=parseInt(cemento);
+    alert("Se necesitan "+cal+" bolsas de cal y "+cemento+" de cemento");
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,46 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    var uno;
+    var dos;
+    var tres;
+    uno=document.getElementById("PrecioUno").value;
+    dos=document.getElementById("PrecioDos").value;
+    tres=document.getElementById("PrecioTres").value;
+    uno=parseInt(uno);
+    dos=parseInt(dos);
+    tres=parseInt(tres);
+    alert(uno+dos+tres);
 }
 function Promedio () 
 {
-	
+    var uno;
+    var dos;
+    var tres;
+    var promedio;
+    uno=document.getElementById("PrecioUno").value;
+    dos=document.getElementById("PrecioDos").value;
+    tres=document.getElementById("PrecioTres").value;
+    uno=parseInt(uno);
+    dos=parseInt(dos);
+    tres=parseInt(tres);
+    promedio=(uno+dos+tres)/3;
+    promedio=parseInt(promedio);
+    alert(promedio);
 }
 function PrecioFinal () 
 {
+    var uno;
+    var dos;
+    var tres;
+    var preciofinal;
+    uno=document.getElementById("PrecioUno").value;
+    dos=document.getElementById("PrecioDos").value;
+    tres=document.getElementById("PrecioTres").value;
+    uno=parseInt(uno);
+    dos=parseInt(dos);
+    tres=parseInt(tres);
+    preciofinal=(uno+dos+tres);
+    alert(preciofinal+preciofinal*21/100)
 	
 }
