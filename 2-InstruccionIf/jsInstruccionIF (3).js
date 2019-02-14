0463ee7bd907a45af7f1748b5f92864b316081bb
Ezedarguibel08 <47418117+Ezedarguibel08@users.noreply.github.com>
2019-02-14 12:18:09 -0300

    if

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad
+    edad=document.getElementById("edad").value;
+    if(edad<18){
+        alert("Usted es menor");
+    }
+    else
+    {
+        if(edad>=18)
+        alert("usted es mayor");
+    }
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad;
+    edad=document.getElementById("edad").value;
+    if(edad>=18) {
+        alert("usted es mayor");
+    }
+    else
+    {
+        if(edad>=13)
+        {
+    
+         alert("usted es adolescente");
+         }else{
+           
+            alert("Usted es menor");}
+    }
+   
 
 
 
