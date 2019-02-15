422389361a12cde4dc9d0813517165ddf17b33c2
alumno <alumno>
2019-02-15 11:57:07 -0300

    if

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,26 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var hola
+	var mensaje;
+	hola=Math.floor((Math.random() * 10) + 1);
+	if(hola<4)
+	{
+		mensaje=" Vamos,la proxima se puede";
+	}else
+	{
+		if(hola>3&&hola<9)
+		{
+			mensaje=" Aprobó";
+		}else
+		{
+			if(hola>8)
+			{
+				mensaje=" Excelente";
+			}
+
+		}
+		alert("Su nota es "+hola+mensaje);
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,7 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
-
+	var hola;
+	hola=Math.floor((Math.random() * 10) + 1);
+	alert(hola);
 }//FIN DE LA FUNCIÓN
