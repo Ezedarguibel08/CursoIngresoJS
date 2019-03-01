439f1a8b91a8f318399ecd8b98d8f0fc7bd927ca
Ezedarguibel08 <47418117+Ezedarguibel08@users.noreply.github.com>
2019-03-01 12:01:46 -0300

    <

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,33 @@
 function mostrar()
-{
+{	
+	var velocidad
+    var combustibles;
+    var contador;
+    var respuesta;
+    var acumulador;
+    var promedio;
+    var tipo;
+    contador = 0;
+    acumulador = 0;
 
+    while (contador < 5) 
+    {
+        velocidad = prompt("ingrese velocidad");
+        velocidad = parseInt(velocidad)
+        tipo = prompt("ingrese tipo de combustible");
+
+
+
+        contador++
+
+        if (velocidad > 0 && velocidad < 250) 
+        {
+
+        }
+        acumulador = acumulador + velocidad;
+        acumulador = parseInt(acumulador);
+       
+	} 
+		promedio=acumulador/5;
+		alert("el promedio es "+promedio);
 }
